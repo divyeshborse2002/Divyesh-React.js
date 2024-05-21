22b22d861a5c887102fd301277f764a9fc8a4fe2
divyeshborse2002 <divyeshborse2002@gmail.com>
2024-05-21 19:51:22 +0530

    hello

---
+++ Event Handler/Event Handler/src/App.jsx
@@ -0,0 +1,30 @@
+
+import React, {  useState } from 'react';
+import Post from './Component/Post';
+import './App.css'
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res)=>res.json())
+    .then((data)=>setPosts(data))
+    .catch((err)=>console.log(err))
+  };
+
+ 
+
+  return (
+    <div>
+      <div className='btn'> <button onClick={fetchPosts}>Fetch Posts</button></div>
+    
+      <div>
+        {posts.map(post => (
+          <Post key={post.id} title={post.title} body={post.body} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
